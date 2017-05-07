62aedb95adee17a1482e2d817b48d13df7980722
Ruan Gabriel G Barros <rggb@icomp.ufam.edu.br>
2017-05-06 23:34:33 -0400

    [FXD] Tab 4 Adjustment

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 #include <iostream>
 
 int main(){
-  meualoc alocador(30,FIRSTFIT);
-  char* alocacoes[30];
-  char command; int address,length;
+    meualoc alocador(30,FIRSTFIT);
+    char* alocacoes[30];
+    char command; int address,length;
 
-  while(std::cin >> command){
-    switch(command){
-      case 'A':
-      std::cin >> address >> length;
-      alocacoes[address] = alocador.aloca(length);
-      break;
-      case 'F':
-      std::cin >> address;
-      alocador.libera(alocacoes[address]);
-      break;
-      case 'M':
-      alocador.imprimeDados();
-      break;
+    while(std::cin >> command){
+        switch(command){
+            case 'A':
+            std::cin >> address >> length;
+            alocacoes[address] = alocador.aloca(length);
+            break;
+            case 'F':
+            std::cin >> address;
+            alocador.libera(alocacoes[address]);
+            break;
+            case 'M':
+            alocador.imprimeDados();
+            break;
+        }
     }
-  }
 }
